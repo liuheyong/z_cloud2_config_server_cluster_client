ed7eccc7639a458ed4af196aeccc93b7514c13e1
liuheyong <17682347237@163.com>
2019-06-13 18:43:37 +0800

    commit

--- src/main/java/com/cloud2/configclucli/ZCloud2ConfigServerClusterClientApplication.java
+++ src/main/java/com/cloud2/configclucli/ZCloud2ConfigServerClusterClientApplication.java
@@ -1,13 +1,15 @@
 package com.cloud2.configclucli;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZCloud2ConfigServerClusterClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZCloud2ConfigServerClusterClientApplication.class, args);
+        new SpringApplicationBuilder(ZCloud2ConfigServerClusterClientApplication.class).web(true).run(args);
     }
 
 }
---
+++ src/main/java/com/cloud2/configclucli/web/TestController.java
@@ -0,0 +1,25 @@
+package com.cloud2.configclucli.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: LiuHeYong
+ * @create: 2019-06-13
+ * @description:
+ **/
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+}
